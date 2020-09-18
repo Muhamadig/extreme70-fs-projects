f469e69d3428a3a97620c7d6020976760c383018
MuhamadI <mohamadi@radware.com>
2020-09-18 16:39:21 +0300

    Add Toggle to tasks section and show uncompleted tasks only checkbox

--- project1-react-mid/src/components/posts/Posts.jsx
+++ project1-react-mid/src/components/posts/Posts.jsx
@@ -1,14 +1,29 @@
+import {
+  faAngleDoubleDown,
+  faAngleDoubleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
 class Posts extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { showPostsList: true };
   }
+  togglePosts = () => {
+    this.setState({ showPostsList: !this.state.showPostsList });
+  };
   render() {
     return (
       <div>
         <div className="posts">
-          <div className="toolbar">
+          <div className="toolbar" onClick={this.togglePosts}>
+            <FontAwesomeIcon
+              icon={
+                this.state.showPostsList
+                  ? faAngleDoubleDown
+                  : faAngleDoubleRight
+              }
+            />
             <h4 className="details-label">Posts</h4>
             <button className="button">Add</button>
           </div>
--- project1-react-mid/src/components/tasks/Tasks.jsx
+++ project1-react-mid/src/components/tasks/Tasks.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import Task from "./Task";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDoubleDown,
+  faAngleDoubleRight,
+} from "@fortawesome/free-solid-svg-icons";
 class Tasks extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { showTasksList: true, showUncompletedOnly: true };
   }
+  toggleTasks = () => {
+    this.setState({ showTasksList: !this.state.showTasksList });
+  };
+  handleUncompletedTasksCheckbox = (e) => {
+    this.setState({ showUncompletedOnly: e.target.checked });
+  };
   render() {
-    let tasks = this.props.tasks;
+    let tasks =
+      this.state.showUncompletedOnly === false
+        ? this.props.tasks
+        : this.props.tasks.filter((task) => task.completed === false);
     let tasksList = tasks.map((task) => {
       return (
         <Task
@@ -16,16 +30,38 @@ class Tasks extends Component {
         />
       );
     });
+
     return (
       <div>
         <div className="tasks">
-          <div className="toolbar">
+          <div className="toolbar" onClick={this.toggleTasks}>
+            <FontAwesomeIcon
+              icon={
+                this.state.showTasksList
+                  ? faAngleDoubleDown
+                  : faAngleDoubleRight
+              }
+            />
             <h4 className="details-label">Todos</h4>
-            <button className="button" onClick={this.props.handleAddNewTaskButtonClick}>
+            <button
+              className="button"
+              onClick={this.props.handleAddNewTaskButtonClick}
+            >
               Add
             </button>
           </div>
-          <div className="tasks-list">{tasksList}</div>
+          <input
+            checked={this.state.showUncompletedOnly}
+            className="checkbox uncompleted-checkbox"
+            type="checkbox"
+            name="uncompletedTasks"
+            id="uncompletedTasks"
+            onChange={this.handleUncompletedTasksCheckbox}
+          />
+          <label htmlFor="uncompletedTasks">Uncompleted Tasks Only</label>
+          {this.state.showTasksList ? (
+            <div className="tasks-list">{tasksList}</div>
+          ) : null}
         </div>
       </div>
     );
