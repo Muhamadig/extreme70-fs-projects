18879680a9c9e0d86e41429988c897bd32fe0c86
MuhamadI <mohamadi@radware.com>
2020-09-19 17:20:24 +0300

    add new Post Css

--- project1-react-mid/src/components/posts/NewPost.jsx
+++ project1-react-mid/src/components/posts/NewPost.jsx
@@ -2,28 +2,39 @@ import React, { Component } from "react";
 class NewPost extends Component {
   constructor(props) {
     super(props);
-    this.state = { title: "" };
+    this.state = { title: "", body: "" };
   }
 
-  getTitleValue = (e) => {
+  getFormValue = (e, key) => {
     let value = e.target.value;
-    this.setState({ title: value });
+    let state = { ...this.state };
+    state[key] = value;
+    this.setState(state);
   };
   render() {
     return (
-      <div className="add-new-task">
-        <div className="title">Add New Task</div>
+      <div className="add-new-post">
+        <div className="title">Add New Post</div>
         <form
-          className="task-form"
-          onSubmit={(e) => this.props.onAdd(e, this.state.title)}
+          className="post-form"
+          onSubmit={(e) => this.props.onAdd(e, { ...this.state })}
         >
           <div>
-            <label htmlFor="tasks-title">Title: </label>
+            <label htmlFor="posts-title">Title: </label>
             <input
               type="text"
-              id="tasks-title"
-              name="tasks-title"
-              onChange={this.getTitleValue}
+              id="posts-title"
+              name="posts-title"
+              onChange={(e) => this.getFormValue(e, "title")}
+            />
+          </div>
+          <div>
+            <label htmlFor="posts-body">Body: </label>
+            <input
+              type="text"
+              id="posts-body"
+              name="posts-body"
+              onChange={(e) => this.getFormValue(e, "body")}
             />
           </div>
 
