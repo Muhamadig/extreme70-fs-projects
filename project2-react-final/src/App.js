4da0d55be8bc1cb72bf29c96f01d68404c662caa
MuhamadI <mohamadi@radware.com>
2020-10-13 22:16:00 +0300

    Welcome Page design

--- project2-react-final/src/App.js
+++ project2-react-final/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './components/welcome/Welcome';
 
 function App() {
   return (
-    <div className="container-fluid App-bg-dark text-white">
-      <Welcome/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
