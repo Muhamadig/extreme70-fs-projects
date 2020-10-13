74acf4ba9b68b74ff49c72de6470a716927d5de7
MuhamadI <mohamadi@radware.com>
2020-10-13 22:45:34 +0300

    Create SignIn Template

--- project2-react-final/src/App.js
+++ project2-react-final/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import SignIn from "./components/signIn/SignIn";
 
 function App() {
   return (
     <div className="container-fluid bg-dark view">
       <Switch>
         <Route path="/signin" component={SignIn} />
+        <Route path="/" component={SignIn} />
       </Switch>
     </div>
   );
---
+++ project2-react-final/src/components/signIn/SignIn.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const SignIn = () => {
+  return <div className="signin-background">Sign</div>;
+};
+
+export default SignIn;
