2c265c31294ea4cab7be299d1ead94e1f7c0de13
MuhamadI <mohamadi@radware.com>
2020-10-13 23:37:00 +0300

    add font awesome and passsword

--- project2-react-final/src/components/signIn/SignIn.jsx
+++ project2-react-final/src/components/signIn/SignIn.jsx
@@ -1,3 +1,12 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import {
+  faKey,
+  faUserAlt,
+  faUserAltSlash,
+  faUserAstronaut,
+  faUserCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
 import "./SignIn.css";
 const SignIn = () => {
@@ -16,13 +25,41 @@ const SignIn = () => {
             <body className="card-body ">
               <form>
                 <div className="form-group">
-                  {/* <label htmlFor="username">Username</label> */}
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    placeholder="Username"
-                  />
+                  <div className="input-group">
+                    <div className="input-group-prepend">
+                      <label
+                        className="input-group-text bg-warning"
+                        htmlFor="username"
+                      >
+                        <FontAwesomeIcon icon={faUserCheck} />
+                      </label>
+                    </div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <small className="form-text text-muted">error message</small>
+                </div>
+                <div className="form-group">
+                  <div className="input-group">
+                    <div className="input-group-prepend">
+                      <label
+                        className="input-group-text bg-warning"
+                        htmlFor="password"
+                      >
+                        <FontAwesomeIcon icon={faKey} />
+                      </label>
+                    </div>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Password"
+                    />
+                  </div>
                   <small className="form-text text-muted">error message</small>
                 </div>
               </form>
