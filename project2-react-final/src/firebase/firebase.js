c20ca0a1aa8d4318cd454885b10019015bf67705
MuhamadI <mohamadi@radware.com>
2020-10-16 16:45:14 +0300

    remove private from git

--- project2-react-final/src/firebase/firebase.js
+++ project2-react-final/src/firebase/firebase.js
@@ -7,16 +7,7 @@ var firestore = require("firebase/firestore");
 const firebaseAdmin = require("firebase-admin");
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBSesGMHhffESLm9aA5stqiWlfQ4mwTENo",
-  authDomain: "movic-project2.firebaseapp.com",
-  databaseURL: "https://movic-project2.firebaseio.com",
-  projectId: "movic-project2",
-  storageBucket: "movic-project2.appspot.com",
-  messagingSenderId: "1096121906798",
-  appId: "1:1096121906798:web:e111db7d44c6e69c43187f",
-  measurementId: "G-DF9S7WMEVM",
-};
+
 //   serviceAccountId:
 //     "firebase-adminsdk-n5tdf@movic-project2.iam.gserviceaccount.com",
 // Initialize Firebase
