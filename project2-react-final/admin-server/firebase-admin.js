8afee237ddc0afb9064ae071cfeafce465fb4882
MuhamadI <mohamadi@radware.com>
2020-10-17 16:18:05 +0300

    init firebase app

--- project2-react-final/admin-server/firebase-admin.js
+++ project2-react-final/admin-server/firebase-admin.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const admin = require("firebase-admin");
 const serviceAccount = require("../private/movic-project2-firebase-adminsdk-n5tdf-dc104cb9e4.json");
-const http = require("http");
 const auth = require("./auth");
 const cors = require("cors");
 
@@ -27,33 +26,3 @@ app.post("/auth/token", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server Started at http://localhost:${port}`);
 });
-// let server = http.createServer(function (req, res) {
-//   let data = [];
-//   let method = req.method;
-//   let url = req.url;
-//   let payload;
-//   req.on("data", (chunk) => {
-//     data.push(chunk);
-
-//     console.log(data);
-//   });
-//   req.on("end", async () => {
-//     console.log(data);
-//     payload = JSON.parse(data);
-//     console.log(payload.uid);
-//     let result = await auth.createCustomToken(
-//       payload.uid,
-//       payload.developerClaims
-//     );
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Origin,X-Requested-With,Content-Type,Accept"
-//     );
-//     res.write(result);
-//     res.end();
-//   });
-// });
-
-// server.listen(9000);
-// console.log("Server started on port 9000");
--- project2-react-final/src/firebase/firebase.js
+++ project2-react-final/src/firebase/firebase.js
@@ -1,23 +1,6 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-var firebase = require("firebase");
-var app = require("firebase/app");
-var analytics = require("firebase/analytics");
-var auth = require("firebase/auth");
-var firestore = require("firebase/firestore");
-const firebaseAdmin = require("firebase-admin");
+const app = require("firebase/app");
+const firebaseConfig = require("../private/firebaseConfig");
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-//   serviceAccountId:
-//     "firebase-adminsdk-n5tdf@movic-project2.iam.gserviceaccount.com",
-// Initialize Firebase
 export let initializeApp = async () => {
-  // let init = firebase.initializeApp(firebaseConfig);
-  // let result = await firebase
-  //   .auth()
-  //   .signInWithCustomToken(
-  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.9OPbM3qMRFJs8NZ3QE1ycbvsnAvWB5QHaCZaEqGFGgc"
-  //   );
-  // console.log(init);
-  // console.log(result);
+  return app.initializeApp(firebaseConfig);
 };
