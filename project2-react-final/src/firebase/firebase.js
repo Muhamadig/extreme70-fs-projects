c3e02b835d5e1a7bd4603566aead27f66fb8db33
MuhamadI <mohamadi@radware.com>
2020-10-24 14:09:07 +0300

    delete to restore

--- project2-react-final/src/firebase/firebase.js
+++
@@ -1,6 +0,0 @@
-const app = require("firebase/app");
-const firebaseConfig = require("../private/firebaseConfig");
-
-export let initializeApp = () => {
-  return app.initializeApp(firebaseConfig);
-};
